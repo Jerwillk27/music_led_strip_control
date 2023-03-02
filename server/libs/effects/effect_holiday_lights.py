2eda59a5864a9a0f5478bba924366153beb6defb
Jerwillk27 <126728486+Jerwillk27@users.noreply.github.com>
2023-03-02 07:37:14 -0800

    Updated Code

--- server/libs/effects/effect_holiday_lights.py
+++ server/libs/effects/effect_holiday_lights.py
@@ -85,5 +85,43 @@ class EffectHolidayLights(Effect):
             if effect_config["interval_enable"]:
                 if self.interval_history > = effect_config["interval"]:
                     if self.alt == 1:
-                        
-                        
+                        output_array[0][start:end] = self._config_colours[effect_config["color_1"]][0]
+                        output_array[1][start:end] = self._config_colours[effect_config["color_1"]][1]
+                        output_array[2][start:end] = self._config_colours[effect_config["color_1"]][2]
+                        self.alt = 2
+                    elif self.alt == 2:
+                        output_array[0][start:end] = self._config_colours[effect_config["color_2"]][0]
+                        output_array[1][start:end] = self._config_colours[effect_config["color_2"]][1]
+                        output_array[2][start:end] = self._config_colours[effect_config["color_2"]][2]
+                        self.alt = 1
+                else:
+                    output_array[0][start:end] = self._config_colours[effect_config["color_1"]][0]
+                    output_array[1][start:end] = self._config_colours[effect_config["color_1"]][1]
+                    output_array[2][start:end] = self._config_colours[effect_config["color_1"]][2]
+                e = 0
+                n = end
+            elif e == 0:
+                # non-lit LEDs
+                start = n
+                end = n + s
+                output_array[0][start:end] = 0
+                output_array[1][start:end] = 0
+                output_array[2][start:end] = 0
+                e = 0
+                n = end
+        
+        if "SK6812" in self._led_strip:
+            output_array[3][:] = effect_config["white"]
+            
+        # Increment and add the current interval to the interval history
+        if self.interval_history >= effect_config["interval"]:
+            self.interval_history = 0
+        else:
+            self.interval_history = self.interval_history + 1
+            
+        # Persist the current value of alt
+        self.alt = self.alt
+        
+        # Add the output array to the queue.
+        self.queue_output_array_blocking(output_array)
+          
